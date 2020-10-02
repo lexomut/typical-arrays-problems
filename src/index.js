d27c24b9277ea473ce431f81ec19844ee9640bd3
lexomut <lexomut@mail.ru>
2020-10-03 00:47:02 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array ) return 0
+    if (array.length==0 || !array ) return 0
+    let result=array[0]
+    for (let i of array){
+        if(result>i) result=i
+    }
+    return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array ) return 0
+    if (array.length==0 || !array ) return 0
+
+    let result=array[0]
+    for (let i of array){
+        if(result<i) result=i
+    }
+    return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array ) return 0
+    if (array.length==0 || !array ) return 0
+
+
+    let result = array.reduce((sum,item)=>sum+item,0)/array.length
+  return result;
 }
